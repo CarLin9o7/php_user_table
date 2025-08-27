ceac31ff90c8c886991c2006871441d1f39ae535
Vei☆ <crmchs.maano.carolyn@gmail.com>
2025-08-27 11:44:21 +0800

    Update index.php

--- new_project/index.php
+++ new_project/index.php
@@ -28,7 +28,49 @@ require_once('connection.php');
 // foreach($sql as $values){
 //     echo $values['CustomerName']."<br>";
 // }
-$stmt = $connect->query("select * from user_table order by user_id DESC ");
+// -------------------------------------------------------------
+// Data to add/update
+$first_name = "Veera";
+$last_name = "Shore";
+$email = "seashore@gmail.com";
+$gender = "Female";
+$user_address = "Bogo City, Cebu";
+$user_id = 50;
+// -------------------------------------------------------------
+// positional parameters
+// $sql = "insert into user_table(first_name, last_name, email, gender, user_address) values(?,?,?,?,?)";
+// $stmt = $connect->prepare($sql);
+// $stmt->execute([$first_name, $last_name, $email, $gender, $user_address]);
+// -------------------------------------------------------------
+// named parameters
+// $sql = "insert into user_table(first_name, last_name, email, gender, user_address) values(:first_name, :last_name, :email, :gender, :user_address)";
+// $stmt = $connect->prepare($sql);
+// $stmt = execute([
+//     'first_name'=>$first_name, 
+//     'last_name'=>$last_name, 
+//     'email'=>$email, 
+//     'gender'=>$gender, 
+//     'user_address'=>$user_address]);
+// -------------------------------------------------------------
+// update table
+// $sql = "update user_table set first_name = :first_name, last_name = :last_name, email = :email, gender = :gender, user_address = :user_address where user_id = :user_id";
+// $stmt = $connect->prepare($sql);
+// $stmt->execute([
+//     'first_name' => $first_name, 
+//     'last_name' => $last_name, 
+//     'email' => $email, 
+//     'gender' => $gender, 
+//     'user_address' => $user_address,
+//     'user_id' => $user_id
+// ]);
+// -------------------------------------------------------------
+$user_id = 50;
+$sql = "select * from user_table where user_id = :user_id";
+$stmt = $connect->prepare($sql);
+$stmt->execute(['user_id' => $user_id]);
+$result = $stmt->fetchAll(PDO::FETCH_OBJ);
+
+// $stmt = $connect->query("select * from user_table order by user_id DESC ");
 ?>
 
 <!DOCTYPE html>
@@ -48,14 +90,21 @@ $stmt = $connect->query("select * from user_table order by user_id DESC ");
             <th>gender</th>
             <th>user_address</th>
         </tr>
-        <?php foreach($stmt as $values): ?>
+        <!-- <php foreach($stmt as $values): ?> -->
+        <?php foreach($result as $values): ?>
         <tr>
-            <td><?php echo $values['user_id'] ?></td>
-            <td><?php echo $values['first_name'] ?></td>
-            <td><?php echo $values['last_name'] ?></td>
-            <td><?php echo $values['email'] ?></td>
-            <td><?php echo $values['gender'] ?></td>
-            <td><?php echo $values['user_address'] ?></td>
+            <!-- <td><php echo $values['user_id'] ?></td>
+            <td><php echo $values['first_name'] ?></td>
+            <td><php echo $values['last_name'] ?></td>
+            <td><php echo $values['email'] ?></td>
+            <td><php echo $values['gender'] ?></td>
+            <td><php echo $values['user_address'] ?></td> -->
+            <td><?php echo $values->user_id ?></td>
+            <td><?php echo $values->first_name ?></td>
+            <td><?php echo $values->last_name ?></td>
+            <td><?php echo $values->email ?></td>
+            <td><?php echo $values->gender ?></td>
+            <td><?php echo $values->user_address ?></td>
         </tr>
         <?php endforeach ?>
     </table>
